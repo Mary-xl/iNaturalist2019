6fbe7518bf80fd4c9a1d7756b5d327e0aaa485c3
Mary Li <xunlimary@hotmail.com>
2019-12-21 21:52:02 +1100

    modify train.py

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ def train_baseline(root_path):
         class_mode="categorical",
         target_size=(IMG_SIZE,IMG_SIZE))
 
-    model=get_model()
+    model=get_model(IMG_SIZE,IMG_SIZE, num_classes)
     optimizer = SGD(lr=LEARNING_RATE, momentum=0.9, decay=0.0, nesterov=True)
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
